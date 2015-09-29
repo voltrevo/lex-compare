375d8453dee66590deaee1169bffa9df8fd0327d
Andrew Morris <voltrevo@gmail.com>
2015-09-29 22:59:30 +1000

    set up linting

--- test/index.js
+++ test/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var assert = require('assert');
-var lexCompare = require('../lib');
+/* global describe it */
 
-describe('lex-compare', function () {
-  it('should have unit test!', function () {
+var assert = require('assert');
+// var lexCompare = require('../lib');
+
+describe('lex-compare', function() {
+  it('should have unit test!', function() {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
